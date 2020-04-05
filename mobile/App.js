4f313d3a9008b84ac61e6d9f3996d8e21815bb14
DevFigueiredo <miranda.f@aluno.ifsp.edu.br>
2020-04-05 15:41:18 -0300

    Recriado o repositório do mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
-//import Routes from './src/routes';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View >
-     </View>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
